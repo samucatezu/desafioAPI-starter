f107ae32114c0f35ee862eca9f8710dcfcf3ed13
Samuel Almeida <barbosasamuel24@gmail.com>
2022-04-18 10:05:01 -0300

    First commit

---
+++ src/main/java/com/gft/desafioapistarter/DesafioApiStarterApplication.java
@@ -0,0 +1,13 @@
+package com.gft.desafioapistarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioApiStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gft/desafioapistarter/DesafioApiStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.desafioapistarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
