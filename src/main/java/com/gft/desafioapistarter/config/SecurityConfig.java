b3d0a0bdfff6df78dddfc687e0a70e274697fab4
Samuel Almeida <barbosasamuel24@gmail.com>
2022-04-21 09:59:49 -0300

    security is working on memory

---
+++ src/main/java/com/gft/desafioapistarter/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.gft.desafioapistarter.config;
+
+import com.gft.desafioapistarter.service.UsuarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UsuarioServiceImpl usuarioService;
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.
+                userDetailsService(usuarioService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf() .disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/api/starters/**", "/api/categoria/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.POST,"/api/starters", "/api/categoria").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE,"/api/starters/**", "/api/categoria/**").hasRole("ADMIN")
+                .and()
+                .httpBasic();
+    }
+}
--- src/main/java/com/gft/desafioapistarter/controller/CategoriaController.java
+++ src/main/java/com/gft/desafioapistarter/controller/CategoriaController.java
@@ -15,7 +15,7 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/categoria")
+@RequestMapping("/api")
 public class CategoriaController {
 
     @Autowired
