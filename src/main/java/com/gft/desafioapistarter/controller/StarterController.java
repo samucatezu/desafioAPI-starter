6f7acbc91c99b09e94e9309ce064be145a3d3591
Samuel Almeida <barbosasamuel24@gmail.com>
2022-04-22 20:58:00 -0300

    File Upload is working!

--- src/main/java/com/gft/desafioapistarter/controller/StarterController.java
+++ src/main/java/com/gft/desafioapistarter/controller/StarterController.java
@@ -6,10 +6,12 @@ import com.gft.desafioapistarter.repository.StartersRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.data.domain.Sort.Order;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -23,12 +25,52 @@ public class StarterController {
     @Autowired
     private StartersRepository startersRepository;
 
+    private Sort.Direction getSortDirection(String direction) {
+        if (direction.equals("asc")) {
+            return Sort.Direction.ASC;
+        } else if (direction.equals("desc")) {
+            return Sort.Direction.DESC;
+        }
+
+        return Sort.Direction.ASC;
+    }
+
     @GetMapping
     @ApiOperation("Retorna uma lista com todos os Starters cadastrados")
     public ResponseEntity<List<Starters>> listarTodos() {
         List<Starters> starters = new ArrayList<>();
         starters = startersRepository.findAll();
         return new ResponseEntity<>(starters, HttpStatus.OK);
+    }
+
+    @GetMapping("/desc")
+    @ApiOperation("Retorna uma lista com todos os Starters cadastrados em ascendente")
+    public ResponseEntity<List<Starters>> listarTodosDesc(@RequestParam(defaultValue = "nome,desc") String[] sort) {
+        try {
+            List<Order> orders = new ArrayList<Order>();
+
+            if (sort[0].contains(",")) {
+                // will sort more than 2 fields
+                // sortOrder="field, direction"
+                for (String sortOrder : sort) {
+                    String[] _sort = sortOrder.split(",");
+                    orders.add(new Order(getSortDirection(_sort[1]), _sort[0]));
+                }
+            } else {
+                // sort=[field, direction]
+                orders.add(new Order(getSortDirection(sort[1]), sort[0]));
+            }
+
+            List<Starters> Starters = startersRepository.findAll(Sort.by(orders));
+
+            if (Starters.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(Starters, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping
---
+++ src/main/java/com/gft/desafioapistarter/repository/FileDBRepository.java
@@ -0,0 +1,11 @@
+package com.gft.desafioapistarter.repository;
+
+import com.gft.desafioapistarter.model.FileDB;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface FileDBRepository extends JpaRepository<FileDB, String> {
+
+}
